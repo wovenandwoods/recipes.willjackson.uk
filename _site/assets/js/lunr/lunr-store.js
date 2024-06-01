4e14b2bbecceb7c9907897d7b786d6b1263d687e
username <username@users.noreply.github.com>
2024-06-01 22:40:10 +0000

    Auto generated from 23f7167

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Mujadara",
+        "excerpt":"Serves 4 Ingredients Olive oil 2 medium onions, thinly sliced 150g brown or green lentils 100g basmati rice 1 tsp ground cumin 1 tsp ground coriander 1/4 tsp ground turmeric 1 tsp ground allspice 1tsp ground cinnamon 1/2 tsp sugar Salt and pepper 175ml water Method Put the lentils in...","categories": ["recipes"],
+        "tags": [],
+        "url": "/recipes/2024/06/01/mujadara.html",
+        "teaser": "/assets/images/gorgzilla.jpeg"
+      },{
+        "title": "Pasta alla Norma",
+        "excerpt":"Serves 2 Ingredients Olive Oil 1 aubergine, cut into 1x1 cm strips 200g pasta 1 tin of tomatoes 3 cloves of garlic, finely chopped 25g basil Ricotta salata (or pecorino) A pinch of dried chili flakes Method Put the sliced aubergine in a colander and sprinkle with salt. Leave to...","categories": ["recipes"],
+        "tags": [],
+        "url": "/recipes/2024/06/01/pasta-alla-norma.html",
+        "teaser": "/assets/images/gorgzilla.jpeg"
+      }]
