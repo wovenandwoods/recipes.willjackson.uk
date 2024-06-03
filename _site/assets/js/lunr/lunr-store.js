48273de5d6acfd159dd06a58a02e57fcfff5e152
username <username@users.noreply.github.com>
2024-06-03 14:46:12 +0000

    Auto generated from 4953349

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,22 @@ var store = [{
         "tags": [],
         "url": "/recipes/2024/06/01/pasta-alla-norma.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
+      },{
+        "title": "Bucatini with Chili",
+        "excerpt":"Serves 2 Ingredients 200g bucatini (can also be done with spaghetti or linguini) 1 large clove garlic 1 fresh red chili (or a pinch of dried chili flakes) 2 anchovy fillets 4 ripe cherry tomatoes Extra virgin olive oil 1 x 180g tin of tuna in olive oil 1 tablespoon...","categories": ["recipes"],
+        "tags": [],
+        "url": "/recipes/2024/06/03/bucatini-with-tuna.html",
+        "teaser": "/assets/images/gorgzilla.jpeg"
+      },{
+        "title": "Chili",
+        "excerpt":"Serves 6 Ingredients 1 tbsp. vegetable oil 1 diced onion 500g minced beef 2 tsp salt 1 tsp ground black pepper 1-2 tbsp. chili powder 1 tbsp. ground cumin 1/8 tsp ground cinnamon 2 tsp paprika 1 tsp unsweetened cocoa powder ¼ tsp dried oregano ¼ tsp ground cayenne 3...","categories": ["recipes"],
+        "tags": [],
+        "url": "/recipes/2024/06/03/chili.html",
+        "teaser": "/assets/images/gorgzilla.jpeg"
+      },{
+        "title": "Shakshuka",
+        "excerpt":"Serves 2 Ingredients 2 tbsp. olive oil 1/2 onion, finely sliced 1 red pepper, diced 3 garlic cloves, crushed 1 tsp. smoked paprika ½ tsp. cumin seeds ½ tsp. cayenne pepper ½ tsp. dried thyme ½ tsp. dried oregano 400g tin of tomatoes 1 tsp. sugar ½ tbsp. lemon juice...","categories": ["recipes"],
+        "tags": [],
+        "url": "/recipes/2024/06/03/shakshuka.html",
+        "teaser": "/assets/images/gorgzilla.jpeg"
       }]
