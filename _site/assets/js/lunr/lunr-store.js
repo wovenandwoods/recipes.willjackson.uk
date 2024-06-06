252cdf87e343b2cb79cfaed69d776a2214cbe27e
username <username@users.noreply.github.com>
2024-06-06 11:46:56 +0000

    Auto generated from d22c5c3

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -30,7 +30,7 @@ var store = [{
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Pasta e Fagioli",
-        "excerpt":"Serves 2 Ingredients 1 tin borlotti beans, drained 1 stick of celery, diced 1 carrot, diced 1 onion, diced Sprig of rosemary Olive oil A few strips of pancetta, diced 1 tin of plum tomatoes 500ml good stock 1 bay leaf A pinch of dried chili flakes 150g small dried...","categories": ["recipes"],
+        "excerpt":"Serves 2 Ingredients 1 tin borlotti beans, drained 1 stick of celery, diced 1 carrot, diced 1 onion, diced 2 cloves of garlic, diced Sprig of rosemary Olive oil A few strips of pancetta, diced 1 tin of plum tomatoes 500ml good stock 1 bay leaf A pinch of dried...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/05/13-00-pasta-e-fagioli.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
