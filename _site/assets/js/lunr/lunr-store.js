b049758ee6099783994bed7800e6b2275626449f
username <username@users.noreply.github.com>
2024-06-19 11:03:48 +0000

    Auto generated from 78898b6

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,66 +1,66 @@
 var store = [{
         "title": "Mujadara",
-        "excerpt":"Serves 4 Ingredients Olive oil 2 medium onions, thinly sliced 150g brown or green lentils 100g basmati rice 1 tsp ground cumin 1 tsp ground coriander 1/4 tsp ground turmeric 1 tsp ground allspice 1tsp ground cinnamon 1/2 tsp sugar Salt and pepper 175ml water Method Put the lentils in...","categories": ["recipes"],
+        "excerpt":"Spice rice and lentils, served with yoghurt and a cucumber salad. Serves 4 Ingredients Olive oil 2 medium onions, thinly sliced 150g brown or green lentils 100g basmati rice 1 tsp ground cumin 1 tsp ground coriander 1/4 tsp ground turmeric 1 tsp ground allspice 1tsp ground cinnamon 1/2 tsp...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/01/mujadara.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Pasta alla Norma",
-        "excerpt":"Serves 2 Ingredients Olive Oil 1 aubergine, cut into 1x1 cm strips 200g pasta 1 tin of tomatoes 3 cloves of garlic, finely chopped 25g basil Ricotta salata (or pecorino) A pinch of dried chili flakes Method Put the sliced aubergine in a colander and sprinkle with salt. Leave to...","categories": ["recipes"],
+        "excerpt":"Pasta in a tomato sauce with cheese and roasted aubergine. Serves 2 Ingredients Olive Oil 1 aubergine, cut into 1x1 cm strips 200g pasta 1 tin of tomatoes 3 cloves of garlic, finely chopped 25g basil Ricotta salata (or pecorino) A pinch of dried chili flakes Method Put the sliced...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/01/pasta-alla-norma.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Bucatini with Tuna",
-        "excerpt":"Serves 2 Ingredients 200g bucatini (can also be done with spaghetti or linguini) 1 large clove garlic 1 fresh red chili (or a pinch of dried chili flakes) 2 anchovy fillets 4 ripe cherry tomatoes Extra virgin olive oil 1 x 180g tin of tuna in olive oil 1 tablespoon...","categories": ["recipes"],
+        "excerpt":"Pasta with tuna, chili and capers. Serves 2 Ingredients 200g bucatini (can also be done with spaghetti or linguini) 1 large clove garlic 1 fresh red chili (or a pinch of dried chili flakes) 2 anchovy fillets 4 ripe cherry tomatoes Extra virgin olive oil 1 x 180g tin of...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/03/bucatini-with-tuna.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Chili",
-        "excerpt":"Serves 6 Ingredients 1 tbsp. vegetable oil 1 diced onion 500g minced beef 2 tsp salt 1 tsp ground black pepper 1-2 tbsp. chili powder 1 tbsp. ground cumin 1/8 tsp ground cinnamon 2 tsp paprika 1 tsp unsweetened cocoa powder ¼ tsp dried oregano ¼ tsp ground cayenne 3...","categories": ["recipes"],
+        "excerpt":"Spicy stew made with mince meat. Serve with rice and tortilla chips. Serves 6 Ingredients 1 tbsp. vegetable oil 1 diced onion 500g minced beef 2 tsp salt 1 tsp ground black pepper 1-2 tbsp. chili powder 1 tbsp. ground cumin 1/8 tsp ground cinnamon 2 tsp paprika 1 tsp...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/03/chili.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Shakshuka",
-        "excerpt":"Serves 2 Ingredients 2 tbsp. olive oil 1/2 onion, finely sliced 1 red pepper, diced 3 garlic cloves, crushed 1 tsp. smoked paprika ½ tsp. cumin seeds ½ tsp. cayenne pepper ½ tsp. dried thyme ½ tsp. dried oregano 400g tin of tomatoes 1 tsp. sugar ½ tbsp. lemon juice...","categories": ["recipes"],
+        "excerpt":"Eggs poached in a spicy tomato and pepper sauce. Serves 2 Ingredients 2 tbsp. olive oil 1/2 onion, finely sliced 1 red pepper, diced 3 garlic cloves, crushed 1 tsp. smoked paprika ½ tsp. cumin seeds ½ tsp. cayenne pepper ½ tsp. dried thyme ½ tsp. dried oregano 400g tin...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/03/shakshuka.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Pasta e Fagioli",
-        "excerpt":"Serves 2 Ingredients 1 tin borlotti beans, drained 1 stick of celery, diced 1 carrot, diced 1 onion, diced 2 cloves of garlic, diced Sprig of rosemary Olive oil A few strips of pancetta, diced 1 tin of plum tomatoes 500ml good stock 1 bay leaf A pinch of dried...","categories": ["recipes"],
+        "excerpt":"Pasta and beans in a tomato sauce. Serves 2 Ingredients 1 tin borlotti beans, drained 1 stick of celery, diced 1 carrot, diced 1 onion, diced 2 cloves of garlic, diced Sprig of rosemary Olive oil A few strips of pancetta, diced 1 tin of plum tomatoes 500ml good stock...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/05/pasta-e-fagioli.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Pasta e Ceci",
-        "excerpt":"Serves 2 Ingredients 1 tin of chickpeas, drained 1 onion 1 stick of celery Olive oil Sprig of rosemary 1 tsp tomato puree 1 bay leaf Pinch of dried chili flakes Parmesan 150g small pasta 500ml stock Method Add the olive oil to a pan and gently fry the onion,...","categories": ["recipes"],
+        "excerpt":"Pasta and chickpeas in a tomato sauce. Serves 2 Ingredients 1 tin of chickpeas, drained 1 onion 1 stick of celery Olive oil Sprig of rosemary 1 tsp tomato puree 1 bay leaf Pinch of dried chili flakes Parmesan 150g small pasta 500ml stock Method Add the olive oil to...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/06/pasta-e-ceci.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Cheddar and Leek Orzotto",
-        "excerpt":"Serves 2 Ingredients 1 large leek, washed and trimmed 20g unsalted butter Splash of olive oil 1 large clove of garlic, finely chopped 150g orzo pasta 500ml chicken or vegetable stock 100g petit pois 80g cheddar Method Halve the leek lengthways, then halve again into quarters and finely slice. Heat...","categories": ["recipes"],
+        "excerpt":"Orzo with cheddar and leeks. Serves 2 Ingredients 1 large leek, washed and trimmed 20g unsalted butter Splash of olive oil 1 large clove of garlic, finely chopped 150g orzo pasta 500ml chicken or vegetable stock 100g petit pois 80g cheddar Method Halve the leek lengthways, then halve again into...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/13/cheddar-and-leek-orzotto.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Gigantes Plaki",
-        "excerpt":"Feeds 2 Ingredients 250g dried gigantes or butter beans 3/4 tsp tbsp flaky salt, plus extra to serve 1 bay leaf 1/2 cinnamon stick 1 garlic clove, peeled 25ml extra-virgin olive oil, plus more to serve 1/2 large onion, peeled and finely chopped 1 celery stick, trimmed and finely chopped...","categories": ["recipes"],
+        "excerpt":"Butter beans in a tomato sauce. Feeds 2 Ingredients 250g dried gigantes or butter beans 3/4 tsp tbsp flaky salt, plus extra to serve 1 bay leaf 1/2 cinnamon stick 1 garlic clove, peeled 25ml extra-virgin olive oil, plus more to serve 1/2 large onion, peeled and finely chopped 1...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/13/gigantes-plaki.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Ciambotta",
-        "excerpt":"Feeds 2 Ingredients 1 onion, sliced 2 peppers (yellow and red), sliced 1 clove of garlic, sliced Half a small red chili, chopped (or a pinch or dried chili flakes) 1 large potato, peeled and cut into wedges 500g tomatoes, diced (or a tin of drained plum tomatoes) 3-5 tbsp...","categories": ["recipes"],
+        "excerpt":"Stewed onions, peppers, tomatoes and potatoes. Feeds 2 Ingredients 1 onion, sliced 2 peppers (yellow and red), sliced 1 clove of garlic, sliced Half a small red chili, chopped (or a pinch or dried chili flakes) 1 large potato, peeled and cut into wedges 500g tomatoes, diced (or a tin...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/18/ciambotta.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
       },{
         "title": "Pasta e Piselli",
-        "excerpt":"Feeds 2 Ingredients 1 onion, diced 1 clove of garlic, diced Pinch of dried chili flakes 100g pasta Handful of frozen peas 1 bunch of mint leaves, roughly chopped Olive oil Pecorino Method Put a pan of salted water on to boil and boil the pasta until just done. Heat...","categories": ["recipes"],
+        "excerpt":"Pasta with peas, garlic and mint. Feeds 2 Ingredients 1 onion, diced 1 clove of garlic, diced Pinch of dried chili flakes 100g pasta Handful of frozen peas 1 bunch of mint leaves, roughly chopped Olive oil Pecorino Method Put a pan of salted water on to boil and boil...","categories": ["recipes"],
         "tags": [],
         "url": "/recipes/2024/06/18/pasta-e-piselli.html",
         "teaser": "/assets/images/gorgzilla.jpeg"
